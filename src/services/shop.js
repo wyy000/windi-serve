661f9ca652ac585daeadc8d7753471287f2c825a
yang <952711716@qq.com>
2021-09-08 11:08:35 +0800

    feat: 添加数据库模型

--- src/services/shop.js
+++ src/services/shop.js
@@ -1,56 +1,36 @@
-// const memoryStorage = {...Array.from({length: 10 * 10000}).map((_, i) => ({[i]: Math.random()}))}
-const memoryStorage = {
-  '1001': { name: '良品铺子' },
-  '1002': { name: '来伊份' },
-  '1003': { name: '三只松鼠' },
-  '1004': { name: '百草味' },
-}
-
-async function delay (ms = 200) {
-  await new Promise((r) => setTimeout(r, ms))
-}
+const {Shop} = require('../models')
 
 class ShopService {
-  async init () {
-    await delay()
-  }
+  async init () {}
 
   async create (values) {
-    await delay()
-
-    const id = String(1 + Object.keys(memoryStorage).reduce((m, id)  => Math.max(m, id), -Infinity))
-
-    return {id, ...(memoryStorage[id] = Object.assign({}, values))}
+    return await Shop.create(values)
   }
 
   async find ({id, pageNum = 1, pageSize = 20}) {
-    await delay()
-
-    if (id) return memoryStorage[id]
+    if (id) return [await Shop.findByPk(id)]
     else {
-      return Object.keys(memoryStorage)
-        .slice((Number(pageNum) - 1) * pageSize, pageNum * pageSize)
-        .map(id => ({id, ...memoryStorage[id]}))
+      return await Shop.findAll({
+        offset: pageNum * pageSize,
+        limit: pageSize,
+      })
     }
   }
 
   async modify ({id, ...values}) {
-    await delay()
-
-    const target = memoryStorage[id]
+    const target = await Shop.findByPk(id)
     if (!target) return null
     else {
-      return Object.assign(target, {id, ...values})
+      Object.assign(target, {id, ...values})
+      return await target.save()
     }
   }
 
   async delete ({id}) {
-    await delay()
-
-    const target = memoryStorage[id]
+    const target = await Shop.findByPk(id)
     if (!target) return false
     else {
-      return delete memoryStorage[id]
+      return target.destroy()
     }
   }
 }
