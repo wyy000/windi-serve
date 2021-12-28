b3c679e53f31647b21c2072ec94431cfca5227d7
yang <952711716@qq.com>
2021-12-28 17:46:02 +0800

    feat: excel接口名称修改

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,13 +2,13 @@ const {Router} = require('express')
 const shopController = require('./shop')
 const chaosController = require('./chaos')
 const loginController = require('./login')
-const execlController = require('./execl')
+const excelController = require('./excel')
 
 module.exports = async function initControllers () {
   const router = Router()
   router.use('/api/shop', await shopController())
   router.use('/api/chaos', await chaosController())
   router.use('/api/login', await loginController())
-  router.use('/api/execl', await execlController())
+  router.use('/api/excel', await excelController())
   return router
 }
